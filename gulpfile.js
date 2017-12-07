95ba5293edade598c3dcb73df4b84346270e9392
CharmedSatyr <z@charmed.tech>
2017-12-08 01:13:44 +0900

    gulpfile automates processing of scss, all js, pug, & browserSync stream

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var pug = require('gulp-pug');
+var pump = require('pump');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+// Compile scss into CSS, autoprefix it, clean and minify it,
+// put the result in the styles folder, & stream updates to the browser.
+gulp.task('scss', function() {
+  return gulp
+    .src('src/scss/*.scss')
+    .pipe(
+      sass({
+        'sourcemap=none': true
+      })
+    )
+    .pipe(concat('styles.min.css'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('styles/'))
+    .pipe(browserSync.stream());
+});
+
+// Concat jquery into one file with src JavaScript, uglify the result, put
+// it into the js folder, and stream updates to the browser
+gulp.task('js', function() {
+  return gulp
+    .src(['node_modules/jquery/dist/jquery.min.js', 'src/js/*.js'])
+    .pipe(concat('index.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream());
+});
+
+// Inject processed css and js into index.pug and render the pug into index.html
+// No need to stream this to the browser because browserSync is watching html
+gulp.task('pug', function() {
+  var target = gulp.src('src/index.pug');
+  var sources = gulp.src(['styles/*.css', 'js/*.js']);
+  return target
+    .pipe(inject(sources))
+    .pipe(pug({ data: {} }))
+    .pipe(concat('index.html'))
+    .pipe(gulp.dest('./'));
+});
+
+// Static server + watching scss/pug/html files
+gulp.task('serve', ['scss', 'js', 'pug'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('src/scss/*.scss', ['scss']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
